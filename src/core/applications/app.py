e0793f7574a8dcedf77f4e4e761e00e655b8b7ad
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-11-21 13:10:46 -0500

    Add Try Exception

--- src/core/applications/app.py
+++ src/core/applications/app.py
@@ -8,11 +8,14 @@ class WhisperImplementation(whisper_pb2_grpc.WhisperModelServicer):
         response = whisper_pb2.SpeechToTextResponse()
         # Assuming request_iterator contains audio data in each request
         for request in request_iterator:
-            print("Received audio data..." + str(len(request.audio)))
-            # Transcribe each audio request
-            transcription = transcribe(audio=request.audio)
-            # Assuming response has a field called 'transcription' to store the result
-            response.text = transcription
+            try:
+                print("Received audio data..." + str(len(request.audio)))
+                # Transcribe each audio request
+                transcription = transcribe(audio=request.audio)
+                # Assuming response has a field called 'transcription' to store the result
+                response.text = transcription
+            except Exception as e:
+                print(f"Error occurred during transcription: {e}")
         return response
 
 
@@ -21,9 +24,12 @@ class PiperServiceImplementation(piper_pb2_grpc.PiperModelServicer):
         response = piper_pb2.TextToSpeechResponse()
         # Assuming request_iterator contains text data in each request
         for request in request_iterator:
-            print("Received text data..." + request.text)
-            # Synthesize each text request
-            audio = speech(text=request.text)
-            # Assuming response has a field called 'audio' to store the result
-            response.audio = audio
+            try:
+                print("Received text data..." + request.text)
+                # Synthesize each text request
+                audio = speech(text=request.text)
+                # Assuming response has a field called 'audio' to store the result
+                response.audio = audio
+            except Exception as e:
+                print(f"Error occurred during synthesis: {e}")
         return response
