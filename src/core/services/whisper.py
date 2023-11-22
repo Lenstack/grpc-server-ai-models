ade3a20d1811bd2266f758b258e0f8bb1372a6d6
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-11-21 20:31:01 -0500

    Update

--- src/core/services/whisper.py
+++ src/core/services/whisper.py
@@ -3,4 +3,6 @@ import whisper
 
 def transcribe(model_name="base", initial_prompt="", audio_file="audio/records.wav", task="translate",
                language="en"):
-    return "Hello World!"
+    audio_model = whisper.load_model(model_name)
+    response = audio_model.transcribe(audio_file, initial_prompt, task, language)
+    return response["text"]
