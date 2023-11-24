84868c43ad2a6875c2e00c8ddc6242e9ff9f6f5a
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-11-24 00:14:57 -0500

    Update whisper.py

--- src/core/services/whisper.py
+++ src/core/services/whisper.py
@@ -1,21 +1,25 @@
 from datetime import datetime
-
 from faster_whisper import WhisperModel
 
 
-def transcribe(model_name="base", initial_prompt="", audio_file="", task="",
-               language="", word_timestamps=False):
-    if audio_file is None:
-        return "No audio file found"
+def transcribe(model_name="base", initial_prompt="", audio_file="../audio/records.wav", task="", language="",
+               word_timestamps=False):
+    if not audio_file:
+        return "No audio file found or provided"
 
-    audio_model = WhisperModel(model_name, device="cpu", compute_type="int8", download_root="../models",
-                               local_files_only=True)
-    segments, _ = audio_model.transcribe("../audio/records.wav", beam_size=5, task=task, language=language,
-                                         word_timestamps=word_timestamps)
-    response = ''.join(segment.text for segment in segments)
+    try:
+        audio_model = WhisperModel(model_name, device="cpu", compute_type="int8", download_root="../models",
+                                   local_files_only=True)
+        segments, _ = audio_model.transcribe(audio_file, beam_size=5, task=task, language=language,
+                                             word_timestamps=word_timestamps)
+        response = ''.join(segment.text for segment in segments)
 
-    # Store the response in a file with the current timestamp
-    with open(f"../audio/records_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as f:
-        f.write(response)
+        # Store the response in a file with the current timestamp
+        output_file = f"../audio/records_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+        with open(output_file, "w") as f:
+            f.write(response)
 
-    return response
+        return response
+
+    except Exception as e:
+        return f"Transcription failed: {str(e)}"
