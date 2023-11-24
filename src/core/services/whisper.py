7b8594c8d9a26d0d45523a51805949376603f8b8
Lenstack <42081538+Lenstack@users.noreply.github.com>
2023-11-24 00:08:19 -0500

    Refactor And Update

--- src/core/services/whisper.py
+++ src/core/services/whisper.py
@@ -1,11 +1,21 @@
-import whisper
+from datetime import datetime
+
+from faster_whisper import WhisperModel
 
 
-def transcribe(model_name="base", initial_prompt="", audio_file="audio/records.wav", task="translate",
-               language="en", word_timestamps=False):
-    audio_model = whisper.load_model(model_name, download_root="../models")
-    audio = whisper.load_audio(audio_file)
+def transcribe(model_name="base", initial_prompt="", audio_file="", task="",
+               language="", word_timestamps=False):
+    if audio_file is None:
+        return "No audio file found"
 
-    response = audio_model.transcribe(audio=audio, fp16=False, task=task, language=language,
-                                      initial_prompt=initial_prompt, word_timestamps=word_timestamps)
-    return response["text"]
+    audio_model = WhisperModel(model_name, device="cpu", compute_type="int8", download_root="../models",
+                               local_files_only=True)
+    segments, _ = audio_model.transcribe("../audio/records.wav", beam_size=5, task=task, language=language,
+                                         word_timestamps=word_timestamps)
+    response = ''.join(segment.text for segment in segments)
+
+    # Store the response in a file with the current timestamp
+    with open(f"../audio/records_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as f:
+        f.write(response)
+
+    return response
